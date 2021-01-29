f47fc336be59d7b4083e1e4aee2f7b3c0a4a54de
iamphlegmatic <abhijit19058@iiitd.ac.in>
2021-01-29 12:03:58 +0530

    first commit

---
+++ 260A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   long long int a,b,n,i;
+    bool flag=true;
+    cin>>a>>b>>n;
+
+    for(int i=0;i<=9;i++)
+    {
+        long long int x=a*10+i;
+        if(x%b==0)
+        {
+            flag=false;
+            cout<<x;
+            break;
+        }
+    }
+    if(flag)
+    cout<<-1;
+    else
+    {
+        for(int i=2;i<=n;i++)
+        cout<<0;
+    }
+    
+}
